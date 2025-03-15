9ada1ad3be0a68c88cef422d9fca66f8d5c86b46
amithg1 <amithg2502@gmail.com>
2025-03-15 18:38:29 +0530

    Modified main.tf, and deploy-glue.yaml. Both to include CW logging.

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,18 @@ resource "aws_iam_policy_attachment" "glue_s3_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
+# Attach CloudWatch Logs permissions to IAM Role
+resource "aws_iam_policy_attachment" "glue_logging" {
+  name       = "glue-logging-policy-attachment"
+  roles      = [aws_iam_role.glue_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+# Create CloudWatch Log Group for Glue Job Logs
+resource "aws_cloudwatch_log_group" "glue_log_group" {
+  name = "/aws-glue/jobs/glue-etl-job"
+}
+
 # RDS MySQL Database
 resource "aws_db_instance" "rds_mysql" {
   allocated_storage    = 20
@@ -45,5 +57,16 @@ resource "aws_glue_job" "glue_etl" {
   command {
     script_location = "s3://${aws_s3_bucket.data_lake.bucket}/glue-scripts/glue_etl.py"
     python_version  = "3"
+  }
+  default_arguments = {
+    "--TempDir"              = "s3://${aws_s3_bucket.data_lake.bucket}/temp/"
+    "--enable-metrics"       = "true"
+    "--enable-continuous-log" = "true"
+    "--enable-glue-datacatalog" = "true"
+    "--log-group-name"         = aws_cloudwatch_log_group.glue_log_group.name
+  }
+
+  execution_property {
+    max_concurrent_runs = 1
   }
 }
